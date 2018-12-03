62c7b1b10871cdee5e07ceea6e8d1ee2b14d4e50
gergely <kiss-gergely@outlook.com>
2018-12-03 20:16:40 +0000

    init commit

---
+++ src/test/java/com/coding2d/basicspringsecurity/BasicSpringSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coding2d.basicspringsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicSpringSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
